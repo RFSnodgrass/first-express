1fa441f83f720fd2c13f83323344758954b61055
rhett <rhett.snodgrass@us.af.mil>
2020-05-20 11:03:46 -0600

    Added more routes

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var path = require('path');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const bodyParser = require("body-parser")
 
 var app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const myStudents = [ { "studentId" : 1, "studentName" : "John Doe", "grades" : [{ "English" : "A" }] },
+{ "studentId" : 2, "studentName" : "Jane Doe", "grades" : [{ "English" : "C" }, { "Math" : "B" }] } ]
+
+function findStudent (studentId) {
+    return myStudents.find(student => student.studentId === Number(studentId))    
+}
+
+function searchStudents (request) {
+    
+    if(request.query.search){
+        return filteredStudents = myStudents.filter(student => student.studentName.includes(request.query.search))        
+    }
+    else{
+        return myStudents
+    }   
+      
+}
+
+function findStudentGrades (studentId) {
+    let foundStudent = myStudents.find(student => student.studentId === Number(studentId))
+    if(foundStudent){
+        return foundStudent.grades
+    }
+    else{
+        return "No Grades Found"
+    }    
+}
+
+app.get('/student', (req, res) => res.json(searchStudents(req)));
+app.get('/students/:studentId', (req, res) => res.json(findStudent(req.params.studentId)));
+app.get('/grades/:studentId', (req, res) => res.json(findStudentGrades(req.params.studentId)));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const classroom = require('../students.js')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => res.send("BOBO"));
 
 module.exports = router;
